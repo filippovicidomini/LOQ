9a2e7095a6effe1c47dca9f0583af765badeb6e9
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-02-28 11:32:02 +0100

    fatto grafico per W(z)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-"""Disegna, usando uno script Python, la dimensione laterale di un fascio gaus- siano in funzione della direzione di propagazione della radiazione.
+"""Disegna, usando uno script Python, la dimensione laterale di un fascio gaussiano in funzione della direzione di propagazione della radiazione.
 Assumi una larghezza minima w0 = 10 μm nel fuoco, e la lunghezza d’onda di un laser a elio-neon nel rosso.
 Di quanto è aumentato il diametro del fascio ad una distanza dal fuoco z = 3 mm?
 Con gli stessi parametri disegna ora l’ampiezza di campo del fascio nel piano in due posizioni: nel fuoco,
 e alla distanza di Rayleigh. Come cam- biano relativamente l’ampiezza massima e la larghezza del fascio tra i due punti?
 E la sua intensità massima, e quella totale?"""
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Definisco le costanti
 w0 = 10e-6
@@ -31,9 +31,12 @@ z = np.linspace(0,3e-3,1000)
 
 # Disegno la dimensione laterale del fascio in funzione della direzione di propagazione
 plt.figure(1)
-plt.plot(theta*180/np.pi,w(z))
+plt.plot(theta*180/np.pi, w(z))
 plt.xlabel('theta [°]')
 plt.ylabel('w [m]')
-plt.title('Dimensione laterale del fascio in funzione della direzione di propagazione')
+plt.title('Dimensione laterale del fascio \n in funzione della direzione di propagazione')
 plt.grid(0.5)
+plt.legend(['w(z)'])
+plt.tight_layout()
 plt.show()
+
